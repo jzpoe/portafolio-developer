0070cbfea5757bca1062c08ecc9025ab6551b103
Jzpoe <pmguez1287@gmail.com>
2024-04-24 07:42:42 -0500

    se agrega nuevo proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Footer from './components/footer/Footer'
 import Home from './components/home/Home'
 import Main from './components/main/Main'
-import { Proyect } from './components/proyects/Proyect'
+import Proyect  from './components/proyects/Proyect'
 import Skils from './components/skils/Skils'
 import { Whatsapp } from './components/whtatsapp/Whatsapp'
 import './reset.css'
--- src/components/proyects/Proyect.jsx
+++ src/components/proyects/Proyect.jsx
@@ -1,35 +1,27 @@
-import { useEffect } from 'react';
-import ScrollReveal from "scrollreveal";
- 
-import './proyect.css'
-export const Proyect = () => {
-  useEffect(() => {
-    const scrollReveal = ScrollReveal();
-    scrollReveal.reveal(".proyectos-h1",  {
-      origin: "left",
-      distance: "50px",
-      duration: 1000,
-      delay: 0,
-      easing: "ease",
-    });
-    
-  }, []);
+import { Fade } from 'react-reveal';
+import './proyect.css';
+
+const Proyect = () => {
   return (
     <>
-    <div className="proyectos">
+      <div className="proyectos">
         <h1 className='proyectos-h1'>proyectos</h1>
-    </div>
-    <div className='contenedor-proyectos'>
-        
-    <a href="https://all-tech-board.vercel.app/" target="_blank">
-    <img src="/assets/proyectos/reparaciones.png" alt="" />
-    </a>
-
-    <a href="https://inventory-two-xi.vercel.app" target="_blank">
-    <img src="/assets/proyectos/inventario.png" alt="" />
-    </a>
-
-    </div>
+      </div>
+      <div className='contenedor-proyectos'>
+        <Fade bottom cascade>
+          <a href="https://all-tech-board.vercel.app/" target="_blank">
+            <img src="/assets/proyectos/reparaciones.png" alt="" />
+          </a>
+          <a href="https://inventory-two-xi.vercel.app" target="_blank">
+            <img src="/assets/proyectos/inventario.png" alt="" />
+          </a>
+          <a href="https://store-full.vercel.app/" target="_blank">
+            <img src="/assets/proyectos/fullStore.png" alt="" />
+          </a>
+        </Fade>
+      </div>
     </>
   )
 }
+
+export default Proyect;
